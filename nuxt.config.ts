fa0288a2bd3161cdc6848d932cb8bd1a22fc565f
KevinHaeusler <kevin.haeusler@khdev.ch>
2023-05-29 09:11:03 +0200

    Integrate Nuxt Color-Mode Module Fixes #3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxt/content','@nuxtjs/tailwindcss'],
+    modules: ['@nuxt/content','@nuxtjs/tailwindcss','@nuxtjs/color-mode'],
     content: {
       documentDriven: true
-    }
+    },
+    colorMode: {    
+    preference: 'system', // default value of $colorMode.preference    
+    fallback: 'light', // fallback value if not system preference found    
+    hid: 'nuxt-color-mode-script',    
+    globalName: '__NUXT_COLOR_MODE__',    
+    componentName: 'ColorScheme',    
+    classPrefix: '',    
+    classSuffix: '',    
+    storageKey: 'nuxt-color-mode'  }
+
   })
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,32 @@
 <template>
-    <div class="bg-blue-800 dark:bg-slate-800">
-  <p class="text-red-500">Index</p>
-    </div>
+  <div class="dark:bg-slate-800 dark:text-gray-100">
+    <h1>Color mode: {{ $colorMode.value }}</h1>
+    <select v-model="$colorMode.preference">
+      <option value="system">System</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+      <option value="sepia">Sepia</option>
+    </select>
+  </div>
 </template>
 
 <script setup>
+const colorMode = useColorMode()
 
+console.log(colorMode.preference)
 </script>
 
-<style lang="scss" scoped>
-
+<style>
+body {
+  background-color: #fff;
+  color: rgba(0,0,0,0.8);
+}
+.dark-mode body {
+  background-color: #091a28;
+  color: #ebf4f1;
+}
+.sepia-mode body {
+  background-color: #f1e7d0;
+  color: #433422;
+}
 </style>
