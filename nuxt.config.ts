85be76f75e97ec736e52389a588214e0372ec739
KevinHaeusler <kevin.haeusler@khdev.ch>
2023-05-29 19:05:05 +0200

    Initial Layout Fixes #11

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxt/content','@nuxthq/ui'],
-    content: {
-      documentDriven: true
+  modules: ['@nuxt/content', '@nuxthq/ui'],
+  content: {
+    documentDriven: true,
+    highlight: {
+      theme: {
+        // Default theme (same as single string)
+        default: 'github-light',
+        // Theme used if `html.dark`
+        dark: 'github-dark',
+      },
     },
-    colorMode: {    
-    preference: 'system', // default value of $colorMode.preference    
-    fallback: 'light', // fallback value if not system preference found    
-    hid: 'nuxt-color-mode-script',    
-    globalName: '__NUXT_COLOR_MODE__',    
-    componentName: 'ColorScheme',    
-    classPrefix: '',    
-    classSuffix: '',    
-    storageKey: 'nuxt-color-mode'  }
+  },
+  colorMode: {
+    preference: 'system', // default value of $colorMode.preference
+    fallback: 'light', // fallback value if not system preference found
+    hid: 'nuxt-color-mode-script',
+    globalName: '__NUXT_COLOR_MODE__',
+    componentName: 'ColorScheme',
+    classPrefix: '',
+    classSuffix: '',
+    storageKey: 'nuxt-color-mode',
+  },
 
-  })
+})
